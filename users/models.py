239d6934edc03ead926a52f45dcaf9cc1b7a19d2
jaypee15 <okoyejohnpaul15@gmail.com>
2023-08-12 14:41:37 +0100

    added country and bio fields to the profile model

--- users/models.py
+++ users/models.py
@@ -1,11 +1,13 @@
-from typing import Iterable, Optional
 from django.db import models
+from django_countries.fields import CountryField
 from django.contrib.auth.models import User
 from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(default='avatar.jpg', upload_to='profile_avatars')
+    country = CountryField( null=True, blank=True)
+    Bio = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
