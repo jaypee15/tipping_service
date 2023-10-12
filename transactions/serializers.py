ee2b9f870488390c1b2a2e472d68ade66d6c09e3
Okoye Johnpaul <okoyejohnpaul15@gmail.com>
2023-10-12 20:55:27 +0100

    Delete transactions directory

--- transactions/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import Transaction
-
-class TransactionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Transaction
-        fields = ("id", "creator", "amount","payment_method","timestamp", "supporter_email")
-        
