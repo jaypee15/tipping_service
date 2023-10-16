158dbbd2c2c618beffdd91f6b3a93faaef8b0356
jaypee15 <okoyejohnpaul15@gmail.com>
2023-10-16 10:27:13 +0100

    fix: Add static_root to core/settings.py

--- core/settings.py
+++ core/settings.py
@@ -143,7 +143,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 MEDIA_ROOT = BASE_DIR / 'media'
