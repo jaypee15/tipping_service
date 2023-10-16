878544c130af64da1eb653bac51237aac8936a41
jaypee15 <okoyejohnpaul15@gmail.com>
2023-10-16 10:51:05 +0100

    fix: modify staticfiles_dirs to build/static

--- core/settings.py
+++ core/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'build/static']
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
