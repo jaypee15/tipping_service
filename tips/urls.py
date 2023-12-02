4da2bf4e9b53b2df7a3b0fc1062c86e3878f7295
jaypee15 <okoyejohnpaul15@gmail.com>
2023-12-02 16:06:38 +0100

    Remove Django files from the main branch

--- tips/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import ListTransactionAPIView,CreateTransactionAPIView
-
-
-urlpatterns = [
-   path('dashboard', ListTransactionAPIView.as_view(), name='dashboard'),
-   path('create', CreateTransactionAPIView.as_view(), name='create'),
-   path("<slug:slug>/", views.MyPageAPi.as_view(), name="mypage"),
-]
