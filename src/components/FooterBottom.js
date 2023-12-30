de4c2ea3af3039d8dc015e57bce9941284e726ca
Adeyemi Eric Ademola <119066166+Ericademola@users.noreply.github.com>
2023-12-30 21:37:02 +0100

    updated the footer eith dynamic year display

--- src/components/FooterBottom.js
+++ src/components/FooterBottom.js
@@ -1,9 +1,10 @@
 import LogoWithName from "../svg/LogoWithName"
 
 const FooterBottom = ({companyName, fill}) => {
+  const currentYear = new Date().getFullYear();
   return (
     <div style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
-        <span style={{color: `${companyName}`}}>&copy; 2023</span> <LogoWithName Kcolor='#fff' width='130' height='40' fill={fill} companyName={companyName}/>
+        <span style={{color: `${companyName}`}}>&copy; {currentYear}</span> <LogoWithName Kcolor='#fff' width='130' height='40' fill={fill} companyName={companyName}/>
     </div>
   )
 }
